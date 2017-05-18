8ae250015a681f39b6300daab2abce9feb1ea4e6
mischa-saunders <mischa.saunders@gmail.com>
2017-05-19 10:12:49 +1200

    added routes

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import {HashRouter as Router,Route} from 'react-router-dom'
+import Alan from './Alan'
+import Elena from './Elena'
+import Nat from './Nat'
+import Gabe from './Gabe'
+
 
 const App = () => {
   return (
-    <h1>React development has begun!</h1>
+    <Router>
+      <div>
+        <h1>React development has begun!</h1>
+        <Route path='/' component={Alan}/>
+        <Route path='/' component={Elena}/>
+        <Route path='/' component={Nat}/>
+        <Route path='/' component={Gabe}/>
+      </div>
+    </Router>
   )
 }
 
 export default App
-
