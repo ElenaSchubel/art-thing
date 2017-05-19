200e178872417c26ecdfc7fcae6104d4b2878b4c
mischa-saunders <mischa.saunders@gmail.com>
2017-05-19 12:23:42 +1200

    randomSounds

---
+++ client/components/Gabe.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import Tone from 'tone'
+
+
+const Gabe = () => {
+  let sounds = ['a4','b4','c4','d4','e5','f4','g4']
+  let soundsN = ['2n', '4n','6n','8n']
+  var synth = new Tone.Synth().toMaster()
+  var grainPlayer = new Tone.GrainPlayer().toMaster()
+
+  const randomSound = () => {
+    let idx = Math.floor(Math.random() * sounds.length - 1)
+    return sounds[idx]
+  }
+
+  const randomOctave = () => {
+    let idx = Math.floor(Math.random() * soundsN.length -1)
+    return soundsN[idx]
+  }
+  const triggerSound = () => {
+    console.log("clicked");
+    synth.triggerAttackRelease(randomSound(), randomOctave())
+    grainPlayer.triggerAttackRelease(randomSound(), randomOctave());
+    // synthBass.triggerAttackRelease('B2', '8n', '1m + 2n + 4n + 8n')
+    // synth.triggerAttackRelease('E4', '8n', '4n + 8n')
+  }
+
+  return (
+    <div>
+      <svg width={1000} height={1000}>
+        <circle onMouseOver={() => triggerSound()} cx={100} cy={100} r={10}/>
+        <circle onMouseOver={() => triggerSound()} cx={200} cy={100} r={10}/>
+        <circle onMouseOver={() => triggerSound()} cx={300} cy={100} r={10}/>
+        <circle onMouseOver={() => triggerSound()} cx={400} cy={100} r={10}/>
+        <circle onMouseOver={() => triggerSound()} cx={500} cy={100} r={10}/>
+        <circle onMouseOver={() => triggerSound()} cx={600} cy={100} r={10}/>
+        <circle onMouseOver={() => triggerSound()} cx={700} cy={100} r={10}/>
+        <circle onMouseOver={() => triggerSound()} cx={800} cy={100} r={10}/>
+        <circle onMouseOver={() => triggerSound()} cx={900} cy={100} r={10}/>
+        <button>Hello</button>
+      </svg>
+    </div>
+  )
+
+}
+
+// <button style={{width: "10vh", height: "10vh"}} ></button>
+
+export default Gabe
